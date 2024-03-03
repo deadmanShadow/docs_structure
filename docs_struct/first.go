6fd33aeb184782031dfd43321289bd7e5615cbc7
Abdullah Raihan Shamil <raihanshamil33@gmail.com>
2024-03-03 21:30:28 +0600

    first go

---
+++ docs_struct/first.go
@@ -0,0 +1,13 @@
+// filename: main.go
+// a program for printing student details
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Raihan Shamil is a student")
+  fmt.Println("Raihan Shamil is 25 years old")
+  fmt.Println("Raihan Shamil has got GPA 4.56 in SSC")
+  fmt.Print("Raihan Shamil originally from Bangladesh");
+}
+
